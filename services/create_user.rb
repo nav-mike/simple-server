7a62bfee0981e24ad87f9669a8d1bdd7385f263d
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-04-07 17:38:25 +0300

    Create create_user.rb

---
+++ services/create_user.rb
@@ -0,0 +1,27 @@
+require_relative '../models/user'
+
+require 'json'
+
+module CreateUser
+  extend self
+
+  def create_user(params)
+    params => email:, name:
+
+    return {error: 'Missing email'} if email.nil?
+    return {error: 'Missing name'} if name.nil?
+
+    user = User.new(id: Time.now.to_i, email:, name:)
+
+    {ok: user}
+  end
+
+  def call(env)
+    case create_user(Hash[JSON.parse(Rack::Request.new(env).body.read).map{ |k, v| [k.to_sym, v] }])
+      in {error: message}
+        [400, { 'Content-Type' => 'application/json' }, [{ error: message }.to_json]]
+      in {ok: user}
+        [200, { 'Content-Type' => 'application/json' }, [{ user: user.to_h }.to_json]]
+    end
+  end
+end
