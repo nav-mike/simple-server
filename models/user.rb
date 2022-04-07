8d02548b7890b944b2f38133d8458c89f01c95ed
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-04-07 17:37:40 +0300

    Create user.rb

---
+++ models/user.rb
@@ -0,0 +1,12 @@
+class User
+  attr_reader :id, :name, :email
+
+  def initialize(id:, name:, email:)
+    super()
+    @id, @name, @email = id, name, email
+  end
+
+  def to_h
+    { id: @id, name: @name, email: @email }
+  end
+end
