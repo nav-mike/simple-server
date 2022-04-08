5e5f8fa3c00a818b050a4d1d2131c80f20408bb5
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-04-08 10:40:04 +0300

    Update create_user.rb

--- services/create_user.rb
+++ services/create_user.rb
@@ -17,11 +17,17 @@ module CreateUser
   end
 
   def call(env)
-    case create_user(Hash[JSON.parse(Rack::Request.new(env).body.read).map{ |k, v| [k.to_sym, v] }])
-      in {error: message}
+    case create_user(params(env))
+      in { error: message }
         [400, { 'Content-Type' => 'application/json' }, [{ error: message }.to_json]]
-      in {ok: user}
+      in { ok: user }
         [200, { 'Content-Type' => 'application/json' }, [{ user: user.to_h }.to_json]]
     end
+  end
+  
+  private
+  
+  def params(env)
+    Hash[JSON.parse(Rack::Request.new(env).body.read).map{ |k, v| [k.to_sym, v] }]
   end
 end
